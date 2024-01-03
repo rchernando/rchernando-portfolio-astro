452acc36e862820a105faa04364a65a727e6e9ca
Roberto Hernando <72400818+rchernando@users.noreply.github.com>
2024-01-03 09:43:27 +0100

    Initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,70 @@
+export const info = {
+    baseUrl: 'https://astro-portfolio-uzair.vercel.app',
+    name: 'Roberto Hernando',
+    jobDescription: 'Fullstack developer',
+    about: `Soy un desarrollador fullstacj con más de 2 años de experiencia, especializado en JavaScript y Angular, trabajando con NestJS / ExpressJS.
+      En backend, tengo experiencia con NodeJS y Express para crear APIs REST. Manejo bases de datos como MySQL, MariaDB y GraphQL. Soy meticuloso en seguir las mejores prácticas y mantener un código impecable, con una perspectiva crítica tanto en 
+      las características del producto como en la dinámica empresarial. ¡Estoy aquí para ayudar en lo que necesites!
+    `,
+
+    experience: [
+        {
+            name: 'Knowmad mood (Remoto)',
+            location: 'Madrid, España',
+            startDate: 'Ago 2023',
+            endDate: 'Actualmente',
+            description: ['✓ Desarrollo web con Angular 16', '✓ Diseño y desarrollo usando NGrx', '✓ Analisis de proyectos'],
+        },
+
+        {
+            name: 'Exceltic (Hibrido)',
+            location: 'Madrid, España',
+            startDate: 'Sept 2021',
+            endDate: 'Ago 2023',
+            description: [
+                `- Desarrollo web con Angular, SQL. Integración de librerías de terceros y desarrollo de gráficas en tiempo real`,
+                `- Diseño y desarrollo de interfaces gráficas y desarrollo de APIs con Python/Node/Java.`,
+                `- Integración continua con Jenkins`,
+                `- Desarrollo web con HTML, CSS y JS modificando una API con Python`,
+                `- Desarrollo con diferentes versiones de Angular (11,14, 15.0 y 15.1)`,
+                `- Desarrollo web con JSP y MariaDB HTML/CSS/TS, Node.JS, PHP, MySQL`,
+            ],
+        },
+    ],
+
+    education: [
+        {
+            name: 'Grado Superior en Desarrollo de Aplicaciones Multiplataforma',
+            location: 'Madrid, España',
+            site: 'Clara del rey',
+            startDate: '2020',
+            endDate: '2022',
+            description: ['Tecnico Superior en Desarrollo de Aplicaciones Multiplataforma'],
+        },
+        {
+            name: 'Grado en Ingenieria de Software',
+            location: 'Madrid, España',
+            site: 'Universidad Politécnica de Madrid',
+            startDate: '2018',
+            endDate: '2020',
+            description: [],
+        }
+    ],
+
+    socialMedia: {
+        twitter: 'https://twitter.com/paanddev',
+        github: 'https://github.com/rchernando',
+        email: 'contact@robertohernando.com',
+        linkedin: 'https://www.linkedin.com/in/roberto-hernando/',
+    },
+
+    projects: [
+        {
+            title: 'Astro Portfolio',
+            isFeatured: true,
+            thumbnail: '/assets/images/astro-portfolio.png',
+            githubUrl: 'https://github.com/uzzii-21/astro-portfolio',
+            liveUrl: 'https://astro-portfolio-uzair.vercel.app/',
+        },
+    ],
+};
